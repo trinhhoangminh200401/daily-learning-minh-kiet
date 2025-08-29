eeb6561cfd557cb6f44ceb4cd7de2e52cbb47053
LuongNhanKiet2020 <74846491+LuongNhanKiet2020@users.noreply.github.com>
2025-08-30 02:14:02 +0700

    day 2: valid_anagrams

---
+++ Kiet_dsa/Array_Hashing/valid_anagram.js
@@ -0,0 +1,13 @@
+const validAnagram = (s, t) => {
+    if(s.length !== t.length){
+        return false
+    }
+
+    const count = new Array(26).fill(0)
+    for(let i = 0; i<s.length; i++){
+        count[s.charCodeAt(i) - 'a'.charCodeAt(0)]++
+        count[t.charCodeAt(i)- 'a'.charCodeAt(0)]--
+    }
+    return count.every(val => val===0)
+}
+validAnagram('racecar', 'carrace')
