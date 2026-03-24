860871e51e47e083a3acb7cb90853953d86b2f85
LuongNhanKiet2020 <74846491+LuongNhanKiet2020@users.noreply.github.com>
2026-03-24 07:59:00 +0700

    monorepo setup

--- microservice/vlc_clone/packages/cms/prisma.config.ts
+++ microservice/vlc_clone/packages/cms/prisma.config.ts
@@ -4,7 +4,8 @@ import { defineConfig } from "prisma/config";
 export default defineConfig({
   schema: "prisma/schema.prisma",
   migrations: {
-    path: "prisma/migrations",
+    path:"prisma/migrations",
+    seed: "prisma/seed.ts",
   },
   datasource: {
     url: process.env.DATABASE_URL,
