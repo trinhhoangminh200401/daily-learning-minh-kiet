c0269802fcf589ba81bc102f5a674dec1aede0e6
LuongNhanKiet2020 <74846491+LuongNhanKiet2020@users.noreply.github.com>
2026-03-24 07:59:00 +0700

    monorepo setup

--- microservice/vlc_clone/packages/cms/lib/prisma.ts
+++ microservice/vlc_clone/packages/cms/lib/prisma.ts
@@ -4,12 +4,12 @@ import { PrismaBetterSqlite3 } from '@prisma/adapter-better-sqlite3'
 const globalForPrisma = globalThis as {
     prisma?: PrismaClient;
 };
-
+const adapter = new PrismaBetterSqlite3({
+    url: process.env.DATABASE_URL ?? 'file:./dev.db'
+})
 export const prisma =
     globalForPrisma.prisma ?? new PrismaClient({
-        adapter: new PrismaBetterSqlite3({
-            url: process.env.DATABASE_URL ?? 'file:./dev.db'
-        })
+        adapter: adapter
     });
 
 if (process.env.NODE_ENV !== 'production') {
